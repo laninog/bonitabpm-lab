9b1a79589ea6696712e6ed80f8cddd59bb163c38
Alejandro Niño <alejandro6230@gmail.com>
2017-10-03 22:39:00 +0200

    Create login component

---
+++ company-app/src/components/login.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <div class="login">
+
+        <h2>My Company</h2>
+
+        <br>
+
+        <label>User</label>
+        <input type="text" v-model="username" />
+
+        <label>Password</label>
+        <input type="password" v-model="password" />
+
+        <button @click="loginBpm({username, password})">Login</button>
+
+    </div>
+
+</template>
+
+<script>
+    import {mapActions} from 'vuex';
+
+    export default {
+        name: 'login',
+        data() {
+            return {
+                username: 'walter.bates',
+                password: 'bpm'
+            };
+        },
+        methods: mapActions(['loginBpm'])
+    }
+</script>
+
+<style>
+
+    .login {
+        text-align: center;
+    }
+
+</style>
